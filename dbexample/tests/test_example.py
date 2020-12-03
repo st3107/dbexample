667fb81bbef8fccdea1001fcad7cfb8b8a81ee25
st3107 <st3107@columbia.edu>
2020-12-03 18:50:42 -0500

    TST: use coverage

--- dbexample/tests/test_example.py
+++ dbexample/tests/test_example.py
@@ -1,7 +1,7 @@
 def test_import():
     from databroker import catalog
 
-    assert "example" in list(catalog)
+    assert "example" in catalog
     db = catalog["example"]
     run = db[-1]
     assert run.metadata
